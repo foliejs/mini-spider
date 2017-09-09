2eed9c83ed4393aae9ad8f29edd9ae1a01a1f75b
sensitiveMix <m18205253820@163.com>
2017-09-09 18:53:05 +0800

    chore: format folder

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = express.Router()
 
 // find user
 router.get('/', (req, res) => {
-  console.log('唐家成')
   res.render('index', {title: 'folie.js'})
 })
 
 router.get('/home', (req, res) => {
-  console.log('唐家成123')
-  res.send('hello tangjiacheng 123')
+  res.send('hello world')
 })
 
 module.exports = router
