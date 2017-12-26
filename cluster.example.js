1e42da7619f54ad2f5fa8bc0074ab8ccd8ba3f3f
sensitiveMix <m18205253820@163.com>
2017-12-26 22:13:30 +0800

    refactor: support cluster arrow

--- cluster.example.js
+++ cluster.example.js
@@ -10,23 +10,23 @@ if (cluster.isMaster) {
     wk.send('[master] ' + 'hi worker' + wk.id)
   }
 
-  cluster.on('fork', function (worker) {
+  cluster.on('fork', (worker) => {
     console.log('[master] ' + 'fork: worker' + worker.id)
   })
 
-  cluster.on('online', function (worker) {
+  cluster.on('online', (worker) => {
     console.log('[master] ' + 'online: worker' + worker.id)
   })
 
-  cluster.on('listening', function (worker, address) {
+  cluster.on('listening', (worker, address) => {
     console.log('[master] ' + 'listening: worker' + worker.id + ',pid:' + worker.process.pid + ', Address:' + address.address + ':' + address.port)
   })
 
-  cluster.on('disconnect', function (worker) {
+  cluster.on('disconnect', (worker) => {
     console.log('[master] ' + 'disconnect: worker' + worker.id)
   })
 
-  cluster.on('exit', function (worker, code, signal) {
+  cluster.on('exit', (worker, code, signal) => {
     console.log('[master] ' + 'exit worker' + worker.id + ' died')
   })
 
@@ -36,26 +36,26 @@ if (cluster.isMaster) {
     }
   }
 
-  setTimeout(function () {
-    eachWorker(function (worker) {
+  setTimeout(() => {
+    eachWorker((worker) => {
       worker.send('[master] ' + 'send message to worker' + worker.id)
     })
   }, 3000)
 
-  Object.keys(cluster.workers).forEach(function (id) {
-    cluster.workers[id].on('message', function (msg) {
+  Object.keys(cluster.workers).forEach((id) => {
+    cluster.workers[id].on('message', (msg) => {
       console.log('[master] ' + 'message ' + msg)
     })
   })
 } else if (cluster.isWorker) {
   console.log('[worker] ' + 'start worker ...' + cluster.worker.id)
 
-  process.on('message', function (msg) {
+  process.on('message', (msg) => {
     console.log('[worker] ' + msg)
     process.send('[worker] worker' + cluster.worker.id + ' received!')
   })
 
-  http.createServer(function (req, res) {
+  http.createServer((req, res) => {
     res.writeHead(200, {'content-type': 'text/html'})
     res.end('worker' + cluster.worker.id + ',PID:' + process.pid)
   }).listen(3000)
